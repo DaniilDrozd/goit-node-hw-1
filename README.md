81ec631828949338df702e5ca01a7024968e7618
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-11-01 16:36:48 +0200

    f

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-node-hw-1
+
+1.node index.js --action="list"- https://ibb.co/dpp2DWH 2. node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6 - https://ibb.co/NKLZHTH
--- contacts.js
+++ contacts.js
@@ -1,6 +1,8 @@
+const commander = require("commander");
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
+
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
